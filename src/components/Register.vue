e5849a70d2dc00cb84fc1b24da9486cec76b99a0
tienpham <quoctienphamm@gmail.com>
2021-09-01 00:53:49 +0700

    push

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,47 +2,52 @@
   <div class="container min-w-full  min-h-screen flex justify-center flex-col  m-auto bg-gradient-to-r from-pink-100 to-blue-200">
     <form
       action=""
-      class="max-w-3xl block m-auto w-6/12"
+      class="lg:max-w-3xl block m-auto lg:w-6/12 w-full"
       @submit.prevent="register">
       <h1 class="font-bold text-2xl text-center mb-3">
-        Đăng ký
+        Đăng ký tài khoản
       </h1>
-      <div>
+      <div class="mx-2">
         <input
           v-model="fullname"
           type="text"
           required
-          class="px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 w-3/6 shadow-sm outline-none block m-auto"
+          class="w-full lg:w-3/6 md:w-3/6 px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3  shadow-sm outline-none block m-auto"
           placeholder="Nhập họ tên..." />
       </div>
-      <div>
+      <div class="mx-2">
         <input
           v-model="username"
           type="text"
           required
-          class="px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 w-3/6 shadow-sm outline-none block m-auto"
+          class="w-full lg:w-3/6 md:w-3/6 px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 shadow-sm outline-none block m-auto"
           placeholder="Nhập tên tài khoản..." />
       </div>
-      <div>
+      <div class="mx-2">
         <input
           v-model="email"
           type="text"
           required
-          class="px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 w-3/6 shadow-sm outline-none block m-auto"
+          class="w-full lg:w-3/6 md:w-3/6 px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 shadow-sm outline-none block m-auto"
           placeholder="Nhập email..." />
       </div>
-      <div>
+      <div class="mx-2">
         <input
           v-model="password"
           required
           type="password" 
-          class="px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 w-3/6 shadow-sm outline-none block m-auto"
+          class="w-full lg:w-3/6 md:w-3/6 px-2 text-md  bg-pink-200 placeholder-white py-2 rounded-md mb-3 shadow-sm outline-none block m-auto"
           placeholder="Nhập password..." />
       </div>
       <div class="flex justify-center">
         <button class="bg-yellow-100 px-5 py-2 rounded-3xl font-semibold shadow-inner border  transition-all outline-none hover:bg-yellow-400">
-          Send
+          Đăng ký
         </button>
+        <router-link
+          :to="{path:'/'}"
+          class="bg-yellow-100 px-5 py-2 rounded-3xl font-semibold shadow-inner border  transition-all outline-none hover:bg-yellow-400">
+          Trở về đăng nhập
+        </router-link>
       </div>
     </form>
   </div>
